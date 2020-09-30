ec1cb300c127bc7c860cb72d4ebed2f72b0208a5
HarjeetSingh <singh.fulda@gmail.com>
2020-09-30 16:02:48 +0200

    Initial commit

---
+++ src/test/java/StoreTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class StoreTest {
+
+    @Test
+    public void testWhenStoreStartsItShouldHaveMenu() {
+
+    }
+
+    @Test
+    public void testWhenStoreStartsItShouldHaveBonusProgramForClient() {
+
+    }
+
+    @Test
+    public void testWhenStoreStartsItShouldAskForOrder() {
+
+    }
+
+}
